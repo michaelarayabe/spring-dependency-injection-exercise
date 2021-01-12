a1816b2dc9d0b2f0f63d2f85593744086baae793
michaelarayabe <michaelseifuaraya@gmail.com>
2021-01-12 16:05:36 +0100

    DI using Spring

--- src/main/java/guru/springframework/sfgdi/controllers/SetterInjectedController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/SetterInjectedController.java
@@ -1,11 +1,16 @@
 package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class SetterInjectedController {
+
 
     public GreetingService greetingService;
 
+    @Autowired
     public void setGreetingService(GreetingService greetingService){
 
         this.greetingService = greetingService;
