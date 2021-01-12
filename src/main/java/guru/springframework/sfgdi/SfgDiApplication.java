14638f964bd0a97381399042e22f2bd80fd49fa1
michaelarayabe <michaelseifuaraya@gmail.com>
2021-01-12 14:42:32 +0100

    DI without using Spring

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,5 +1,6 @@
 package guru.springframework.sfgdi;
 
+import guru.springframework.sfgdi.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -8,8 +9,10 @@ import org.springframework.context.ApplicationContext;
 public class SfgDiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SfgDiApplication.class, args);
-
+		ApplicationContext ap = SpringApplication.run(SfgDiApplication.class, args);
+		MyController myController = (MyController)ap.getBean("myController");
+		myController.sayHello();
+		System.out.println(myController.sayHello());
 	}
 
 }
