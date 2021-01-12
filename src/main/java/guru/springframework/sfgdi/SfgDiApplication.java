e892959385631235f5b767c3b53ae3e8c0377be1
michaelarayabe <michaelseifuaraya@gmail.com>
2021-01-12 10:40:50 +0100

    spring context

---
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -0,0 +1,15 @@
+package guru.springframework.sfgdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SfgDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgDiApplication.class, args);
+
+	}
+
+}
